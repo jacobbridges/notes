33776f0aa343a02f5d5e6a0187ca89a15263d6ec
Jason Carney <jason.jay.carney@gmail.com>
2019-10-06 13:16:42 -0500

    add quick nav for note labels

--- src/notes/selectors.tsx
+++ src/notes/selectors.tsx
@@ -3,6 +3,19 @@ import snakeCase from 'lodash/snakeCase'
 
 export const getState = state => state.notes
 
+export const getLabels = createSelector([getState], state => Object.keys(state).sort())
+
+export const getPaths = createSelector([getLabels], labels => {
+    const specificPaths = {'home': '/'}
+    const specificLabels = Object.keys(specificPaths)
+    return labels.map(label => {
+        if (specificLabels.includes(label)) {
+            return {label: label, path: specificPaths[label]}
+        }
+        return {label: label, path: `/p/${label}/`}
+    })
+})
+
 export const makeGetNoteByLabel = label => {
     return createSelector([getState], (state) => state[snakeCase(label)])
 }
